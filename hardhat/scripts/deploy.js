aa79111905d5a4e80dc1e583af3473372575af2d
lucachaco <luiscarlosh@gmail.com>
2025-03-29 17:20:48 +0100

    fix: update deployment script for Ethers v6 compatibility and correct script path

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -4,8 +4,8 @@ async function main() {
     const Greeter = await hre.ethers.getContractFactory("Greeter");
     const greeter = await Greeter.deploy("Hello world!");
 
-    await greeter.deployed();
-    console.log("Greeter deployed to:", greeter.address);
+    await greeter.waitForDeployment(); // Ethers v6
+    console.log("Greeter deployed to:", await greeter.getAddress()); // Ethers v6
 }
 
 main().catch((error) => {
